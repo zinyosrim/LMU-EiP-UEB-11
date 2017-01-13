9e11ba812292295763d7c14a8b05eb62102e8293
Zin Yosrim <zinyosrim@gmail.com>
2017-01-13 18:33:27 +0100

    mit throw try catch

--- src/Musiker.java
+++ src/Musiker.java
@@ -4,16 +4,37 @@
  */
 public class Musiker {
 
-    // Instanzvariable für die Anzahl der erstellten Musiker^
-    public static int instances;
-    // Attribute
-    private String instrument;
     private String name;
-    // Konstruktor
-    public Musiker(String instrument){
-        this.instrument = instrument;
-        Musiker.instances++;
-        System.out.println("Musiker Nr."+Musiker.instances+" mit Instrument: " + this.instrument+" wurde erstellt.");
+
+    // Instanzvariable für die Anzahl der erstellten Musiker
+    private static int instanceCounter=0;
+
+    private Musiker(String name){
+    // String className = new Exception().getStackTrace()[1].getClassName();
+            this.name = name;
+            instanceCounter++;
     }
 
+    public String toString(){
+        return this.name;
+    }
+
+    public static Musiker erstelleMusiker(String name) throws Exception {
+        Musiker m = new Musiker(name);
+        try {
+            if (instanceCounter > 4)
+            {
+                throw new Exception();
+            }
+            else {
+                System.out.println("counter = "+instanceCounter);
+                return m;
+
+            }
+        } catch (Exception e) {
+            System.out.println("Can’t have more than 4 instances");
+        }
+    return m;
+
+    }
 }
--- src/Quartett.java
+++ src/Quartett.java
@@ -1,34 +1,24 @@
-/**
- * Created by ZY on 13.01.17.
- */
-public final class Quartett {
+public class Quartett {
+    private Musiker[] besetzung = new Musiker[4];
 
-    final int ANZAHL_MUSIKER = 4;
-    private Musiker[] musikerGruppe = new Musiker[ANZAHL_MUSIKER];
+    public static void main(String[] args) {
+        Quartett q = new Quartett();
+        q.engagiere("Heinz");
+        q.engagiere("Gustav");
+        q.engagiere("Balduin");
+        q.engagiere("Nepumuk");
+        q.engagiere("Ralf");
+    }
 
-    // Konstruktor
-    Quartett(Musiker[] musikerArray){
-
-        try{
-            for (int i = 0; i < musikerArray.length; i++){
-               this.musikerGruppe[i] = musikerArray[i];
-            }
-            System.out.println("Quartett ist erstellt.");
-        } catch(Exception e){
-            System.out.println("Quartett konnte nicht angelegt werden. Die Anzahl der Musiker darf 4 nicht überschreiten!");
+    private void engagiere(String name) {
+        try {
+            Musiker m = Musiker.erstelleMusiker(name);
+            System.out.println(m + " spielt nun im Quartett!");
         }
-    }
-
-    public static void main(String[] args){
-
-        Musiker[] albanBergQuartett = new Musiker[5];
-        albanBergQuartett[0] = new Musiker("Klavier");
-        albanBergQuartett[1] = new Musiker("Cello");
-        albanBergQuartett[2] = new Musiker("Violine");
-        albanBergQuartett[3] = new Musiker("Kontrabass");
-        albanBergQuartett[4] = new Musiker("Becken");
-        Quartett q = new Quartett(albanBergQuartett);
-
+        catch(Exception e) {
+            System.out.println(e);
+        }
 
     }
+
 }
