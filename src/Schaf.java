2da5bb8ca3e302bc00f1c781c4d62d9e40b82557
Zin Yosrim <zinyosrim@gmail.com>
2017-01-16 21:53:02 +0100

    vollständige funktionalität fertig

---
+++ src/Schaf.java
@@ -0,0 +1,24 @@
+/**
+ * Klasse Schaf
+ */
+public class Schaf extends Einheit {
+
+    public Schaf(){
+        super();
+    }
+
+    void werdeAngegriffen(int schaden) throws Exception {
+
+        try{
+            System.out.println("Schaf läuft herum und sagt määäh...");
+        }
+
+        finally{
+            int verbleibendeLebenspunkte = this.lebensPunkte-(int)(zielSchadensKorrekturFaktor*schaden);
+            if (verbleibendeLebenspunkte <= 0){
+                this.lebensPunkte = 0;
+            }
+            this.lebensPunkte = verbleibendeLebenspunkte;
+        }
+    }
+}
